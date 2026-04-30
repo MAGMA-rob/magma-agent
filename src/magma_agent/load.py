aaf531ef6834b8c0ca519e7b9fd3d66b63e7ff15
loan <l.bernat@sileane.com>
2026-04-30 15:05:43 +0200

    executable right

--- src/magma_agent/load.py
+++ src/magma_agent/load.py
@@ -12,13 +12,13 @@ def load_commander(
 ):
     if commander_id != "none":
         normalized_commander_id = commander_id.lower()
-        if "original-qwen" in normalized_commander_id:
+        if "qwen" in normalized_commander_id:
             print(f"[MAGMA AGENT] Loading QWEN Commander Model : {commander_id}")
             return QwenCommander(commander_id, cpu_load=optimize_memory, **(qwen_options or {}))
-        elif "original-smollm" in normalized_commander_id:
+        elif "smollm" in normalized_commander_id:
             print("[MAGMA AGENT] Loading SmolLM3")
             return SmolLMCommander(commander_id, optimize_memory)
-        elif "gpt-oss" in normalized_commander_id or "gpt_oss" in normalized_commander_id:
+        elif "oss" in normalized_commander_id or "gpt_oss" in normalized_commander_id:
             print(f"[MAGMA AGENT] Loading GPT OSS Commander Model : {commander_id}")
             return OSSCommander(commander_id, cpu_load=optimize_memory)
         else:
